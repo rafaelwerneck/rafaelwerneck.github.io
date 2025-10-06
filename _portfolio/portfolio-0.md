f67c5009d890637d22188fbde00cdee80342183b
rafaelwerneck <rafaelwerneck25@gmail.com>
2025-10-06 14:30:21 -0300

    Fixed opening in a new tab for all links. Adding DOI link

---
+++ _portfolio/portfolio-0.md
@@ -0,0 +1,5 @@
+---
+title: "Coming soon"
+# excerpt: "Short description of portfolio item number 1<br/><img src='/images/500x300.png'>"
+collection: portfolio
+---
--- assets/js/open-links-new-tab.js
+++ assets/js/open-links-new-tab.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".author__urls a");
+  const links = document.querySelectorAll("a");
   links.forEach(link => {
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
+    // Ensure the link is external
+    if (link.href && !link.href.startsWith(location.origin) && !link.href.startsWith("#") && !link.href.startsWith("javascript:")) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
   });
 });

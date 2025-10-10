251ac4d098201be6541b19274fd23ae1b31fc408
rafaelwerneck <rafaelwerneck25@gmail.com>
2025-10-10 18:38:58 +0000

    Open PDF in another tab and don't show PDF link in the paper single page description.

--- assets/js/open-links-new-tab.js
+++ assets/js/open-links-new-tab.js
@@ -2,9 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("a");
   links.forEach(link => {
     // Ensure the link is external
-    if (link.href && !link.href.startsWith(location.origin) && !link.href.startsWith("#") && !link.href.startsWith("javascript:")) {
-      link.setAttribute("target", "_blank");
-      link.setAttribute("rel", "noopener noreferrer");
+    if (link.href && 
+        !link.href.startsWith("#") && 
+        !link.href.startsWith("javascript:")
+       ) {
+        
+        let shouldOpenInNewTab = false;
+
+        // 1. Check for external links
+        if (!link.href.startsWith(location.origin)) {
+            shouldOpenInNewTab = true;
+        }
+
+        // 2. Check for PDF links (local or external)
+        if (link.href.toLowerCase().endsWith(".pdf")) {
+            shouldOpenInNewTab = true;
+        }
+
+        if (shouldOpenInNewTab) {
+            link.setAttribute("target", "_blank");
+            link.setAttribute("rel", "noopener noreferrer");
+        }
     }
   });
 });
